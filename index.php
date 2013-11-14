ac3a9698e88710aa6b7abf29e0854945fe302c8d
crlome <crlomes@gmail.com>
2013-11-13 21:57:50 -0600

    inicio

--- index.php
+++ index.php
@@ -1,20 +1,46 @@
-<html>
+﻿<html>
 	<head>
-		<link rel='stylesheet' type='text/css' href='/css/foundation.min.css'>
-	    <link rel='stylesheet' type='text/css' href='/css/font-awesome.css'>
-	    <link rel='stylesheet' type='text/css' href='/css/style.css'>
+		<link rel='stylesheet' type='text/css' href='css/foundation.min.css'>
+	    <link rel='stylesheet' type='text/css' href='css/font-awesome.css'>
+	    <link rel='stylesheet' type='text/css' href='css/normalize.css'>
+	    <link rel='stylesheet' type='text/css' href='css/style.css'>
 
-	    <script type='text/javascript' src='/js/jquery-2.0.3.min.js'></script>
-	    <script type='text/javascript' src='/js/foundation.min.js'></script>
-	    <script type='text/javascript' src='/js/lodash.underscore.min.js'></script>
-	    <script type='text/javascript' src='/js/backbone-min.js'></script>
-	    <script type='text/javascript' src='/js/mousetrap.min.js'></script>
-	    <script type='text/javascript' src='/js/mousetrap-bind-dictionary.min.js'></script>
-	    <script type='text/javascript' src='/js/jquery.keyfilter.js'></script>
-	    <script type='text/javascript' src='/js/handlebars.js'></script>
-	    <script type='text/javascript' src='/js/cApp.js'></script>
+	    <script type='text/javascript' src='js/jquery-2.0.3.min.js'></script>
+	    <script type='text/javascript' src='js/foundation.min.js'></script>
+	    <script type='text/javascript' src='js/lodash.underscore.min.js'></script>
+	    <script type='text/javascript' src='js/backbone-min.js'></script>
+	    <script type='text/javascript' src='js/mousetrap.min.js'></script>
+	    <script type='text/javascript' src='js/mousetrap-bind-dictionary.min.js'></script>
+	    <script type='text/javascript' src='js/jquery.keyfilter.js'></script>
+	    <script type='text/javascript' src='js/handlebars.js'></script>
+	    <script type='text/javascript' src='js/cApp.js'></script>
 	</head>
 	<body>
-		<label>hola</label>
+		<h1>Sistema para el control de nomina</h1>
+
+		<form class="panel radius small-6 small-offset-3">
+			<div class="row collapse">
+				<div class="small-3 columns">
+					<label class="prefix">Usuario:</label>
+				</div>
+				<div class="small-9 columns">
+					<input type="text" placeholder="Usuario..." />
+				</div>
+			</div>
+			<div class="row collapse">
+				<div class="small-3 columns">
+					<label class="prefix">Contraseña:</label>
+				</div>
+				<div class="small-9 columns">
+					<input type="text" placeholder="Contraseña..."/>
+				</div>
+			</div>
+			<div class="row collapse">
+				<div class="small-12 columns">
+					<button type="button" class="btn small expand">Iniciar sesion</button>
+				</div>
+			</div>
+		</form>
+
 	</body>
 </hmtl>
