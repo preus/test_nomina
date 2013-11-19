312b25614f7f8228db904c766033bff03f5ad3f4
crlome <crlomes@gmail.com>
2013-11-18 22:31:20 -0600

    menux

---
+++ menu.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<link rel='stylesheet' type='text/css' href='css/foundation.min.css'>
+	    <link rel='stylesheet' type='text/css' href='css/font-awesome.css'>
+	    <link rel='stylesheet' type='text/css' href='css/normalize.css'>
+	    <link rel='stylesheet' type='text/css' href='css/style.css'>
+
+	    <script type='text/javascript' src='js/jquery-2.0.3.min.js'></script>
+	    <script type='text/javascript' src='js/foundation.min.js'></script>
+	    <script type='text/javascript' src='js/lodash.underscore.min.js'></script>
+	    <script type='text/javascript' src='js/backbone-min.js'></script>
+	    <script type='text/javascript' src='js/mousetrap.min.js'></script>
+	    <script type='text/javascript' src='js/mousetrap-bind-dictionary.min.js'></script>
+	    <script type='text/javascript' src='js/jquery.keyfilter.js'></script>
+	    <script type='text/javascript' src='js/handlebars.js'></script>
+	    <script type='text/javascript' src='js/cApp.js'></script>
+	</head>
+	<body>
+		<nav class="top-bar">
+			<section class="top-bar-section right">
+				<ul>
+					<li class="has-dropdown not-click">
+						<a class="top-bar-title" href="#">Usuario</a>
+						<ul class="dropdown">
+							<li><a href="#">Cerrar sesion</a></li>
+						</ul>
+					</li>
+				</ul>
+			</section>
+			<section class="top-bar-section">
+				<ul>
+					<li><a href="#">Escuelas</a></li>
+					<li><a href="#">Consulta</a></li>
+					<li><a href="#">Contrato nuevo</a></li>
+					<li class="has-dropdown not-click">
+						<a href="#">Altas</a>
+						<ul class="dropdown">
+							<li><a href="#">Nuevas</a></li>
+							<li><a href="#">Recientes</a></li>
+						</ul>
+					</li>
+					<li class="has-dropdown not-click">
+						<a href="#">Bajas</a>
+						<ul class="dropdown">
+							<li><a href="#">Recientes</a></li>
+							<li><a href="#">Asesores</a></li>
+						</ul>
+					</li>
+					<li class="has-dropdown not-click">
+						<a href="#">Asesores</a>
+						<ul class="dropdown">
+							<li><a href="#">Nomina RL PIP</a></li>
+							<li><a href="#">Nomina RD PIP</a></li>
+							<li><a href="#">Nomina RL PNIEB</a></li>
+							<li><a href="#">Nomina RD PNIEB</a></li>
+						</ul>
+					</li>
+				</ul>
+			</section>
+		</nav>
+	</body>
+</html>
